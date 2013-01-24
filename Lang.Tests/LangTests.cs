4e971727596ec4ad17032debe909e8809483647e
anton <akropp@gmail.com>
2013-01-24 13:51:26 -0500

    better unit tests

--- Lang.Tests/LangTests.cs
+++ Lang.Tests/LangTests.cs
@@ -86,18 +86,18 @@ namespace Lang.Tests
         public void FunctionTest()
         {
             var test = @"void foo(int x, int y){ 
-                    int x = 1; 
-                    fun() -> { 
-                        zinger = ""your mom!"";
-                        someThing(a + b) + 25 - (""test"" + 5);
-                    };
-                }
+                            int x = 1; 
+                            var z = fun() -> { 
+                                zinger = ""your mom!"";
+                                someThing(a + b) + 25 - (""test"" + 5);
+                            };
+                        }
 
-                z = 3;
+                        z = 3;
 
-                int testFunction(){
-                    var p = 23;
-                }";
+                        int testFunction(){
+                            var p = 23;
+                        }";
 
             var ast = new Parser(new Tokenizer(test)).Parse();
         }
