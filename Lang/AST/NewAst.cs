b8b75962feb7fe1837234fe2722cd9bceb7eec96
anton <akropp@gmail.com>
2013-02-08 11:06:40 -0500

    adding new parsing, class parsing, starting rework of scope trees to allow for class scopes as well as global scopes

---
+++ Lang/AST/NewAst.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Lang.Data;
+using Lang.Visitors;
+
+namespace Lang.AST
+{
+    public class NewAst : Ast
+    {
+        public NewAst(Ast name, List<Ast> args) : base(name.Token)
+        {
+            Args = args;
+            Name = name;
+        }
+
+        public List<Ast> Args { get; set; }
+
+        public Ast Name { get; set; }
+
+        public override void Visit(IAstVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public override AstTypes AstType
+        {
+            get { return AstTypes.New; }
+        }
+    }
+}
---
+++ Lang/Spaces/ScopeContainer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lang.Spaces
+{
+    public enum ScopeType
+    {
+        Class,
+        Global
+    }
+    public class ScopeContainer
+    {
+        public ScopeStack<Scope> Global { get; set; }
+
+        public ScopeStack<Scope> Class { get; set; } 
+
+        public ScopeContainer()
+        {
+            Global = new ScopeStack<Scope>();
+
+            Class = new ScopeStack<Scope>();
+
+            CurrentScopeType = ScopeType.Global;
+        }
+
+        public ScopeType CurrentScopeType { get; set; } 
+
+        public ScopeStack<Scope> CurrentScopeStack
+        {
+            get
+            {
+                switch (CurrentScopeType)
+                {
+                    case ScopeType.Class:
+                        return Class;
+                    case ScopeType.Global:
+                        return Global;
+                }
+
+                return null;
+            }
+        } 
+    }
+}
