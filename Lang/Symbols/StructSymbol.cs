90b0f0427711e3b1115788248b175a16eb25bed2
anton <akropp@gmail.com>
2013-02-08 16:57:05 -0500

    class spaces now working

---
+++ Lang/Symbols/StructSymbol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lang.Symbols.Symbosl2;
+
+namespace Lang.Symbols
+{
+    public class StructSymbol : ISymbol
+    {
+        public String Name { get; set; }
+
+        public Dictionary<String, ISymbol> Symbols { get; set; }
+    }
+}
